c02976666fb675419d88d80ad8655ca49046fdd5
Caleb <calebcoecode@gmail.com>
2020-09-19 19:44:10 -0600

    Add build directory to git for Heroku to display index.js file

---
+++ coe-portfolio/build/precache-manifest.c40d6240ae8051dc253de02b8483325f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db8299f90610b19b82faa02460fde96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6671fee532ed610303",
+    "url": "/static/css/main.f5b5b7f9.chunk.css"
+  },
+  {
+    "revision": "a7f8d56c6c0eae36bb70",
+    "url": "/static/js/2.122e795e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.122e795e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6671fee532ed610303",
+    "url": "/static/js/main.ab8e554f.chunk.js"
+  },
+  {
+    "revision": "697826e0b7f446903264",
+    "url": "/static/js/runtime-main.a7d8b9f9.js"
+  },
+  {
+    "revision": "40a6f2c59cf8b332b1a9bd82a4babda7",
+    "url": "/static/media/FCCDrumMachine.40a6f2c5.png"
+  },
+  {
+    "revision": "180fa0e34a4c82d7ed956b1c8757fe8e",
+    "url": "/static/media/FCCLandingPage.180fa0e3.png"
+  },
+  {
+    "revision": "614cc10bd9b6c3b6bfbfe38b83b716dd",
+    "url": "/static/media/FCCQuoteMachine.614cc10b.png"
+  },
+  {
+    "revision": "b07c8d48a10798165e274e0960be8518",
+    "url": "/static/media/FCCSurvey.b07c8d48.png"
+  },
+  {
+    "revision": "ce8df2a47d19322641e9759b303d61e6",
+    "url": "/static/media/FCCTribute.ce8df2a4.png"
+  },
+  {
+    "revision": "16a7f83a594aad3badae8b51f06311f9",
+    "url": "/static/media/JSGuessGame.16a7f83a.png"
+  },
+  {
+    "revision": "3f98b66c89d3fbd0c3007e74f6d33e60",
+    "url": "/static/media/NodeWeatherApp.3f98b66c.png"
+  }
+]);
